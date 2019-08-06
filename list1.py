8b35210fcdcc650ee65ed11f9581b4b52eb86659
Zachary Kline <zkline888@gmail.com>
2019-08-06 10:05:51 -0400

    Added the finished version of list1.py and list2.py

--- list1.py
+++ list1.py
@@ -23,8 +23,12 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+     count = 0
+     for word in words:
+          if len(word) >= 2:
+               if word[0] == word[-1]:
+                    count += 1
+     return count
 
 
 # B. front_x
@@ -35,8 +39,14 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+     x_words = []
+     rest = []
+     for word in words:
+          if word.startswith('x'):
+               x_words.append(word)
+          else:
+               rest.append(word)
+     return sorted(x_words) + sorted(rest)
 
 
 # C. sort_last
@@ -46,8 +56,8 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    """Your code goes here.  Edit this docstring."""
-    return
+     solution = sorted(tuples, key= lambda x: x[-1])
+     return solution
 
 
 # Simple provided test() function used in main() to print
